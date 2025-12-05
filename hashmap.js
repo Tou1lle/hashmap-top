2e6ace52a3d37d4108ee6d04243704bdd307a52f
Tuan Ha <hatuan@email.cz>
2025-12-05 22:25:56 +0100

    Inital variables

--- hashmap.js
+++ hashmap.js
@@ -1,5 +1,10 @@
 function HashMap() {
-  const bucket = Array(16).fill([]);
+  let loadFactor = 0.75;
+  let capacity = 16;
+  const buckets = Array(capacity).fill([]);
+
+  console.log(buckets, loadFactor, capacity);
 }
 
+HashMap();
 export { HashMap };
