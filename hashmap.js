7c217f653598296f86903f994f46cf8bf6b11f0a
Tuan Ha <hatuan@email.cz>
2025-12-05 22:40:57 +0100

    Add simple hash function

--- hashmap.js
+++ hashmap.js
@@ -3,8 +3,19 @@ function HashMap() {
   let capacity = 16;
   const buckets = Array(capacity).fill([]);
 
-  console.log(buckets, loadFactor, capacity);
+  const hash = (key) => {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  };
+
+  return { hash }
 }
 
-HashMap();
+console.log(HashMap().hash("monkey"));
 export { HashMap };
