d2507089180b7973dadad5147ac2dad1a2735b4b
Tuan Ha <hatuan@email.cz>
2025-12-06 14:33:23 +0100

    Create set function + bucket and entry
    The hash function uses module in each iteration to
    prevent integer overflow. Created functions
    to get the relevant bucket by index and loop over
    a bucket to get the relevant entry
    
    Also changes the Array().fill() - added map cause
    fill creates 1 same instance -> the array is one
    reference

--- hashmap.js
+++ hashmap.js
@@ -1,7 +1,7 @@
 function HashMap() {
   let loadFactor = 0.75;
   let capacity = 16;
-  const buckets = Array(capacity).fill([]);
+  let buckets = Array(capacity).fill().map(u => []);
 
   const hash = (key) => {
     let hashCode = 0;
@@ -9,13 +9,46 @@ function HashMap() {
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode = primeNumber * hashCode + key.charCodeAt(i);
+      hashCode %= capacity;
     }
 
     return hashCode;
   };
 
-  return { hash }
+  function getBucket(key) {
+    const index = hash(key) % capacity;
+    return buckets[index];
+  };
+
+  function getEntry(bucket, key) {
+    for (const entry of bucket) {
+      if (entry.key === key) {
+        return entry;
+      }
+    }
+
+    return null;
+  };
+
+  function set(key, value) {
+    const bucket = getBucket(key);
+    const entry = getEntry(bucket, key);
+    if (entry) {
+      entry.value = value;
+      return;
+    }
+    bucket.push({key, value});
+  };
+
+  const logMap = () => {
+    console.log(buckets);
+  }
+  
+  return { hash, logMap, set };
 }
 
-console.log(HashMap().hash("monkey"));
+const hm = HashMap();
+hm.set("monkey", "bananas");
+hm.set("gorilla", "melons");
+hm.logMap();
 export { HashMap };
